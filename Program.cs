9a5e0b325ae4ee36f21ed4c740db9e64f65e4665
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-18 21:14:13 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//  While Loops | C# | Tutorial 18
+
+
+namespace CSharpExercises_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int index = 1;
+            //while (index <= 5)
+            //{
+            //    Console.WriteLine(index);
+            //    index++;
+
+            //}
+
+
+            // do while loop will run at least 1 time 
+            int index = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+
+            } while (index <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
